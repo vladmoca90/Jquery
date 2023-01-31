a303073e71f8474eef6f371739b5797968bf9e7c
Vlad Mocanu <vladmoca90@gmail.com>
2023-01-31 23:13:39 +0000

    Corrected the JSON for stations

--- scripts/JQuery/train-tickets/train-tickets.js
+++ scripts/JQuery/train-tickets/train-tickets.js
@@ -1,126 +1,125 @@
 const data = {
-    stations: [
+    "stations": [
         {
-            code: "APF",
-            name: "Appleford",
+            "code": "APF",
+            "name": "Appleford"
         },
         {
-            code: "BSK",
-            name: "Basingstoke",
+            "code": "BSK",
+            "name": "Basingstoke"
         },
         {
-            code: "BTH",
-            name: "Bath Spa",
+            "code": "BTH",
+            "name": "Bath Spa"
         },
         {
-            code: "BHM",
-            name: "Birmingham Grand Central",
+            "code": "BHM",
+            "name": "Birmingham Grand Central"
         },
         {
-            code: "BAW",
-            name: "Blackwater",
+            "code": "BAW",
+            "name": "Blackwater"
         },
         {
-            code: "BCE",
-            name: "Bracknell",
+            "code": "BCE",
+            "name": "Bracknell"
         },
         {
-            code: "BRI",
-            name: "Bristol Temple Meads",
+            "code": "BRI",
+            "name": "Bristol Temple Meads"
         },
         {
-            code: "CDF",
-            name: "Cardiff Central",
+            "code": "CDF",
+            "name": "Cardiff Central"
         },
         {
-            code: "DID",
-            name: "Didcot Parkway",
-
+            "code": "DID",
+            "name": "Didcot Parkway"
         },
         {
-            code: "EAL",
-            name: "Ealing Broadway",
+            "code": "EAL",
+            "name": "Ealing Broadway"
         },
         {
-            code: "FNN",
-            name: "Farnborough North",
+            "code": "FNN",
+            "name": "Farnborough North"
         },
         {
-            code: "GTW",
-            name: "Gatwick Airport",
+            "code": "GTW",
+            "name": "Gatwick Airport"
         },
         {
-            code: "GLD",
-            name: "Guildford",
+            "code": "GLD",
+            "name": "Guildford"
         },
         {
-            code: "PAD",
-            name: "London Paddington",
+            "code": "PAD",
+            "name": "London Paddington"
         },
         {
-            code: "WAT",
-            name: "London Waterloo",
+            "code": "WAT",
+            "name": "London Waterloo"
         },
         {
-            code: "MAI",
-            name: "Maidenhead",
+            "code": "MAI",
+            "name": "Maidenhead"
         },
         {
-            code: "MIA",
-            name: "Manchester Picadilly",
+            "code": "MIA",
+            "name": "Manchester Picadilly"
         },
         {
-            code: "NBY",
-            name: "Newbury",
+            "code": "NBY",
+            "name": "Newbury"
         },
         {
-            code: "NCL",
-            name: "Newcastle",
+            "code": "NCL",
+            "name": "Newcastle"
         },
         {
-            code: "OXF",
-            name: "Oxford",
+            "code": "OXF",
+            "name": "Oxford"
         },
         {
-            code: "PLY",
-            name: "Plymouth",
+            "code": "PLY",
+            "name": "Plymouth"
         },
         {
-            code: "RDG",
-            name: "Reading",
+            "code": "RDG",
+            "name": "Reading"
         },
         {
-            code: "SOU",
-            name: "Southampton Central",
+            "code": "SOU",
+            "name": "Southampton Central"
         },
         {
-            code: "TWI",
-            name: "Twickenham",
+            "code": "TWI",
+            "name": "Twickenham"
         },
         {
-            code: "WIN",
-            name: "Winchester",
+            "code": "WIN",
+            "name": "Winchester"
         },
         {
-            code: "WNC",
-            name: "Windsor",
+            "code": "WNC",
+            "name": "Windsor"
         },
         {
-            code: "WKM",
-            name: "Wokingham",
-        },
-    ],
+            "code": "WKM",
+            "name": "Wokingham"
+        }
+    ]
 }
 
 $(function () {
     const addStations = data => {
-        if(data.stations.length == 0) {
+        if (data.stations.length == 0) {
             throw new Error('The stations list must be provided');
         }
 
         let stationContainer = $('.content-stations');
 
-        for(let i = 0; i < data.stations.length; i++) {
+        for (let i = 0; i < data.stations.length; i++) {
             let stationOption = $('<div class="content-stations__col"></div>');
 
             stationOption.val(data.stations[i].name);
