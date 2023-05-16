acd5399afa9250b76c75cc26b330f9bff374d542
Vlad Mocanu <vladmoca90@gmail.com>
2023-05-16 15:16:23 +0100

    Testing

--- scripts/JQuery/cookies/cookie-popup.js
+++ scripts/JQuery/cookies/cookie-popup.js
@@ -6,6 +6,6 @@ $(function() {
     $('.close-btn').on('click', function() {
         $('.pop-up').css('display', 'none');
 
-        Cookies.set('adDismissed', true, { domain: 'file:///C:/Users/vladm/Documents/Visual%20Studio%202015/Projects/Jquery/sites/pop-up.html' });
+        Cookies.set('adDismissed', true, { domain: 'file:///C:/Users/vladm/OneDrive/Desktop/Projects/Jquery/sites/cookie-popUp.html' });
     });
 });
