95264ee454e331a7d343e54e9e755b2a3c90dd01
Vlad Mocanu <vladmoca90@gmail.com>
2023-07-31 23:39:53 +0100

    Will try to the clone the entire html with the X button

--- scripts/JQuery/to-do-list.js
+++ scripts/JQuery/to-do-list.js
@@ -2,7 +2,7 @@ $(function () {
     let addBtn = $('.add-button');
     let list = $('.list-main');
     let text = $('#field');
-    let close = $('<i class="fa fa-close"></i>');
+    //let close = $('<i class="fa fa-close"></i>');
 
     addBtn.on('click', function() {
         let clone = list.find('li.list-item:first').clone();
@@ -11,7 +11,7 @@ $(function () {
         let listText = clone.text(listContent);
 
         list.append(listText);
-        listText.append(close);
+        //listText.append(close);
 
         if(listContent.trim().length == 0) {
             clone.remove();
@@ -19,5 +19,9 @@ $(function () {
         if(listContent.length > 0) {
             return text.val(null);
         }   
+
+        close.on('click', function() {
+            $(this).parent('.list-item').remove();
+        });
     });
 });
