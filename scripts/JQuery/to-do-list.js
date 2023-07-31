b7c01715e27489da3c6467e67228302ac733108f
Vlad Mocanu <vladmoca90@gmail.com>
2023-08-01 00:17:30 +0100

    will keep the close variable for now

--- scripts/JQuery/to-do-list.js
+++ scripts/JQuery/to-do-list.js
@@ -2,7 +2,7 @@ $(function () {
     let addBtn = $('.add-button');
     let list = $('.list-main');
     let text = $('#field');
-    //let close = $('<i class="fa fa-close"></i>');
+    let close = $('<i class="fa fa-close"></i>');
 
     addBtn.on('click', function() {
         let clone = list.html().trim();
