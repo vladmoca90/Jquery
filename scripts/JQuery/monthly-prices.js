ab3b566e946453324c5a2f8108d402a75df2a2c1
Vlad Mocanu <vladmoca90@gmail.com>
2023-08-01 23:19:13 +0100

    Formatted the code

--- scripts/JQuery/monthly-prices.js
+++ scripts/JQuery/monthly-prices.js
@@ -2,15 +2,15 @@ $(function () {
     let vehicles = $('.vehicle-price');
 
     const getVehiclesPrices = vehicles => {
-        if(vehicles.length == 0) {
+        if (vehicles.length == 0) {
             return;
         }
 
-        for(let i = 0; i < vehicles.length; i++) {
+        for (let i = 0; i < vehicles.length; i++) {
             let vehicle = $(vehicles[i]);
             let vehPrice = vehicle.children('.car-price').html().replace(',', '').trim().substring(1);
             let price = parseInt(vehPrice);
-            let monthlyPrice = (price/12).toFixed(2);
+            let monthlyPrice = (price / 12).toFixed(2);
 
             vehicle.children('.monthly-price').text('£' + monthlyPrice + '/month');
         }
