b44bb72f7aa7047e7800f2ede52486b7bec1b665
Vlad Mocanu <vladmoca90@gmail.com>
2023-08-02 00:18:21 +0100

    Formatted the code

--- scripts/JQuery/dropdowns/capitals.js
+++ scripts/JQuery/dropdowns/capitals.js
@@ -1,58 +1,58 @@
 const data = {
     countries: [{
-            name: 'India',
-            code: 'IND',
-            capital: 'Delhi',
-            otherCities: ['Madras', 'Mumbai', 'Calcutta', 'Varanasi', 'Hyderabad'],
-        },
-        {
-            name: 'Romania',
-            code: 'ROM',
-            capital: 'Bucharest',
-            otherCities: ['Cluj-Napoca', 'Iasi', 'Craiova', 'Brasov'],
-        },
-        {
-            name: 'Israel',
-            code: 'ISR',
-            capital: 'Jerusalem',
-            otherCities: ['Tel-Aviv', 'Haifa', 'Eilat', 'Netanya'],
-        },
-        {
-            name: 'France',
-            code: 'FRA',
-            capital: 'Paris',
-            otherCities: ['Lyon', 'Marseille', 'Toulouse', 'Bordeaux', 'Strasbourg', 'Nice'],
-        },
-        {
-            name: 'Moldova',
-            code: 'MDV',
-            capital: 'Kishinev',
-            otherCities: ['Balti', 'Tighina'],
-        },
-        {
-            name: 'Brazil',
-            code: 'BRA',
-            capital: 'Brasilia',
-            otherCities: ['Rio de Janeiro', 'Sao Paulo', 'Fortaleza', 'Recife'],
-        },
-        {
-            name: 'South Africa',
-            code: 'SAF',
-            capital: 'Pretoria',
-            otherCities: ['Johannesburg', 'Cape Town', 'Durban'],
-        },
-        {
-            name: 'Argentina',
-            code: 'ARG',
-            capital: 'Buenos Aires',
-            otherCities: ['Cordoba', 'Mendoza', 'Bahia Blanca', 'Mar del Plata'],
-        },
-        {
-            name: 'United States',
-            code: 'USA',
-            capital: 'Washington DC',
-            otherCities: ['New York City', 'Los Angeles', 'Miami', 'Chicago', 'San Francisco', 'Dallas', 'Seattle', 'Boston', 'Denver'],
-        },
+        name: 'India',
+        code: 'IND',
+        capital: 'Delhi',
+        otherCities: ['Madras', 'Mumbai', 'Calcutta', 'Varanasi', 'Hyderabad'],
+    },
+    {
+        name: 'Romania',
+        code: 'ROM',
+        capital: 'Bucharest',
+        otherCities: ['Cluj-Napoca', 'Iasi', 'Craiova', 'Brasov'],
+    },
+    {
+        name: 'Israel',
+        code: 'ISR',
+        capital: 'Jerusalem',
+        otherCities: ['Tel-Aviv', 'Haifa', 'Eilat', 'Netanya'],
+    },
+    {
+        name: 'France',
+        code: 'FRA',
+        capital: 'Paris',
+        otherCities: ['Lyon', 'Marseille', 'Toulouse', 'Bordeaux', 'Strasbourg', 'Nice'],
+    },
+    {
+        name: 'Moldova',
+        code: 'MDV',
+        capital: 'Kishinev',
+        otherCities: ['Balti', 'Tighina'],
+    },
+    {
+        name: 'Brazil',
+        code: 'BRA',
+        capital: 'Brasilia',
+        otherCities: ['Rio de Janeiro', 'Sao Paulo', 'Fortaleza', 'Recife'],
+    },
+    {
+        name: 'South Africa',
+        code: 'SAF',
+        capital: 'Pretoria',
+        otherCities: ['Johannesburg', 'Cape Town', 'Durban'],
+    },
+    {
+        name: 'Argentina',
+        code: 'ARG',
+        capital: 'Buenos Aires',
+        otherCities: ['Cordoba', 'Mendoza', 'Bahia Blanca', 'Mar del Plata'],
+    },
+    {
+        name: 'United States',
+        code: 'USA',
+        capital: 'Washington DC',
+        otherCities: ['New York City', 'Los Angeles', 'Miami', 'Chicago', 'San Francisco', 'Dallas', 'Seattle', 'Boston', 'Denver'],
+    },
     ],
 }
 
@@ -61,11 +61,11 @@ $(function () {
     let countries = $('#countriesDrop');
 
     const populateCountriesDrop = data => {
-        if(data.length == 0) {
+        if (data.length == 0) {
             throw new Error('The list must be given');
         }
 
-        for(let i = 0; i < data.countries.length; i++) {
+        for (let i = 0; i < data.countries.length; i++) {
             let option = $('<option></option>');
 
             option.attr('value', data.countries[i].code);
@@ -80,8 +80,8 @@ $(function () {
         let selectedOption = $(this).find(':selected');
         let capital = selectedOption.val();
 
-        for(let i = 0; i < data.countries.length; i++) {
-            if(data.countries[i].code == capital) {
+        for (let i = 0; i < data.countries.length; i++) {
+            if (data.countries[i].code == capital) {
                 $('#capital').html(data.countries[i].capital);
             }
         }
@@ -96,10 +96,10 @@ $(function () {
         let selectedOption = $(this).find(':selected');
         let country = selectedOption.val();
 
-        for(let i = 0; i < data.countries.length; i++) {
-            
-            if(data.countries[i].code == country) {
-                for(let j = 0; j < data.countries[i].otherCities.length; j++) {
+        for (let i = 0; i < data.countries.length; i++) {
+
+            if (data.countries[i].code == country) {
+                for (let j = 0; j < data.countries[i].otherCities.length; j++) {
                     let option = $('<option></option>');
                     option.attr('value', data.countries[i].code);
                     option.html(data.countries[i].otherCities[j]);
