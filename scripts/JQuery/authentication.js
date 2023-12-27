8695dbff0c3968738f53df4b8b96252663893646
Vlad Mocanu <vladmoca90@gmail.com>
2023-12-27 22:50:51 +0000

    All compiled

---
+++ scripts/JQuery/authentication.js
@@ -0,0 +1,27 @@
+$(function() {
+    let name = $('#name').val();
+    let email = $('#email').val();
+    let password = $('#password').val();
+    let gender = $('#gender:checked');
+    let checkOut = $('#check:checked');
+
+    $('.validation-messages p').css('display', 'block');
+
+    $('#submit').on('click', function() {
+        if(name.length < 3) {
+            $('.name-alert').css('display', 'block');
+        }
+        if(email.length < 6 && email.indexOf('@') == -1) {
+            $('.email-alert').css('display', 'block');
+        }
+        if(password.length < 6) {
+            $('.password-alert').css('display', 'block');
+        }
+        if(gender.length == 0) {
+            $('.gender-alert').addClass('show-alert');
+        }
+        if(checkOut.length = 0) {
+            $('.checkOut-alert').addClass('show-alert');
+        }
+    });
+});
